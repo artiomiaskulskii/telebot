dc58908d1a7bc49afb117d9f2dead4ea6250346f
artiomiaskulskii <140312155+artiomiaskulskii@users.noreply.github.com>
2025-04-06 13:13:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,19 +11,27 @@ API_TOKEN = token
 
 bot = telebot.TeleBot(API_TOKEN)
 
-@bot.message_handler(commands=["start"])
-def start(message):
-    reply_keyboard_markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    reply_keyboard_markup.row(KeyboardButton("Start MiniApp", web_app=WebAppInfo(WEB_URL)))
 
-    inline_keyboard_markup = InlineKeyboardMarkup()
-    inline_keyboard_markup.row(InlineKeyboardButton('Start MiniApp', web_app=WebAppInfo(WEB_URL)))
-
-    bot.reply_to(message, "Click the bottom inline button to start MiniApp", reply_markup=inline_keyboard_markup)
-    bot.reply_to(message, "Click keyboard button to start MiniApp", reply_markup=reply_keyboard_markup)
-
-@bot.message_handler(content_types=['web_app_data'])
-def web_app(message):
-    bot.reply_to(message, f'Your message is "{message.web_app_data.data}"')
-
-bot.infinity_polling()
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+@bot.message_handler(commands=['info','information'])
+def send_information(message):
+    bot.reply_to(message, """\ 
+Привет! Я бот-дизайнер, и я здесь, чтобы помочь тебе с творческими задачами и идеями для дизайна. Какой у тебя проект? Вот несколько вещей, с которыми я могу помочь:
+1. **Логотипы**: Нужен уникальный логотип для твоего бизнеса? Опиши свою идею, и я помогу сформировать концепцию!
+2. **Цветовые палитры**: Сомневаешься в цветах? Я предложу несколько гармоничных цветовых схем, которые подойдут для твоего проекта.
+3. **Шрифты**: Ищешь идеальные шрифты для своего текста? Я подскажу, какие сочетания будут выглядеть стильно и современно.
+4. **Макеты**: Нужна помощь с созданием макета для веб-сайта или приложения? Я помогу с идеями по структуре и компоновке.
+5. **Идеи и вдохновение**: Ищешь свежие идеи? Я могу предложить тренды в дизайне и показать успешные примеры работ.
+Просто дай мне знать, что именно тебе нужно, и я постараюсь помочь!
+ умею выполянть множество команд:
+/start
+/help
+/info
+\
+""")
